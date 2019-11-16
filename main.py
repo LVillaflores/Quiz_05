5c18dbe3e373400eb7c8388ca282b70ef3fe517b
LCVillaflores <52596850+LCVillaflores@users.noreply.github.com>
2019-11-16 10:24:45 +0800

    Added Code for File reading and Text Files

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-with open('file_name.txt') as file_name:
-	if "String" in file_name.read():
-		print('String is in File')
+Script = open('Script.txt', encoding = 'utf-8')
+
+String = Script.readlines()
+print(String)
+
+# with open('file_name.txt') as file_name:
+# 	if "String" in file_name.read():
+# 		print('String is in File')
